9d96fb08f6ef2b4ad9e01614ad8e6b7b1ba7b3a2
ROMAIN LEMONNIER <lemonnier.romain13@gmail.com>
2025-05-13 11:19:14 +0200

    fix error 3

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+	return (
+		<>
+			<title>Portfolio Romain Lemonnier</title>
+			<meta name="description" content="Découvrez mon profil et mes travaux" />
+			<meta name="color-scheme" content="light only" />
+			<link rel="icon" href="/favicon_rl.png" />
+		</>
+	);
+}
